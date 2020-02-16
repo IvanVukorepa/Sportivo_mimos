c48d96b8672249b29dc46715a61e70f47a637f17
Ivan Vukorepa <ivan.vukorepa132@gmail.com>
2020-02-17 00:37:41 +0100

    commit

--- Sportivo_webAPI/Migrations/20200216220853_SportivoMigration.Designer.cs
+++ Sportivo_webAPI/Migrations/20200216220853_SportivoMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sportivo_webAPI.DAL;
 
 namespace Sportivo_webAPI.Migrations
 {
     [DbContext(typeof(SportivoContext))]
-    partial class SportivoContextModelSnapshot : ModelSnapshot
+    [Migration("20200216220853_SportivoMigration")]
+    partial class SportivoMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Sportivo_webAPI/Models/Company.cs
+++ Sportivo_webAPI/Models/Company.cs
@@ -16,7 +16,7 @@ namespace Sportivo_webAPI.Models
         public string PhoneNumber { get; set; }
         public string Description { get; set; }
         public ICollection<Court> Courts { get; set; }
-        public DateTime? Opens { get; set; }
-        public DateTime? Closes { get; set; }
+        public int Opens { get; set; }
+        public int Closes { get; set; }
     }
 }
